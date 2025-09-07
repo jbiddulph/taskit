35bb51bb4eb085996b0ea81d7430e9212097bf64
John <john.mbiddulph@gmail.com>
2025-09-08 00:08:09 +0100

    Update favicon to use ZapTask branding - blue Z icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,9 +37,10 @@
 
         <title inertia>{{ config('app.name', 'ZapTask') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/zap_icon.png" sizes="32x32" type="image/png">
+        <link rel="icon" href="/zap_icon.png" sizes="192x192" type="image/png">
+        <link rel="apple-touch-icon" href="/zap_icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
