8a84f3f44862e8229913380fd9d2d7e071cabd4c
John <john.mbiddulph@gmail.com>
2025-09-07 17:54:16 +0100

    Fix custom domain asset loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS in production
+        if (config('app.force_https', false)) {
+            URL::forceScheme('https');
+        }
+        
+        // Handle custom domain asset URLs
+        // If accessed via custom domain, use that domain for assets too
+        if (request()->getHost() !== 'task-it-9b20e77d6638.herokuapp.com') {
+            $currentUrl = request()->getScheme() . '://' . request()->getHost();
+            URL::forceRootUrl($currentUrl);
+            config(['app.url' => $currentUrl]);
+        }
     }
 }
