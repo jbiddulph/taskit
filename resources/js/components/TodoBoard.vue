9562122cf58b7d408946fe770896252076da5ec7
John <john.mbiddulph@gmail.com>
2025-09-09 16:33:56 +0100

    Implement icon-only calendar and search/filter toggle buttons
    - Convert calendar button from text to icon-only with tooltip
    - Add new search/filter toggle button with SlidersHorizontal icon
    - Hide search and filters section by default
    - Add toggle functionality to show/hide search filters
    - Improve UI density with compact icon buttons
    - Better organization of filter controls with on-demand visibility

--- resources/js/components/TodoBoard.vue
+++ resources/js/components/TodoBoard.vue
@@ -116,16 +116,24 @@
 
           <button
             @click="showCalendar = !showCalendar"
-            class="inline-flex items-center gap-2 px-4 py-2 bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-100 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 border border-gray-300 dark:border-gray-600"
+            :title="showCalendar ? 'Hide Calendar' : 'Show Calendar'"
+            class="inline-flex items-center justify-center w-10 h-10 bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-100 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 border border-gray-300 dark:border-gray-600"
           >
             <Icon name="Calendar" class="w-4 h-4" />
-            {{ showCalendar ? 'Hide Calendar' : 'Show Calendar' }}
+          </button>
+
+          <button
+            @click="showFilters = !showFilters"
+            :title="showFilters ? 'Hide Filters' : 'Show Filters'"
+            class="inline-flex items-center justify-center w-10 h-10 bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-100 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 border border-gray-300 dark:border-gray-600"
+          >
+            <Icon name="SlidersHorizontal" class="w-4 h-4" />
           </button>
         </div>
       </div>
 
       <!-- Search and Filters -->
-      <div class="mt-4 flex flex-col lg:flex-row gap-4">
+      <div v-if="showFilters" class="mt-4 flex flex-col lg:flex-row gap-4">
         <div class="flex-1 relative">
           <Icon name="Search" class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
           <input
@@ -525,6 +533,7 @@ const showForm = ref(false);
 const showCreateProject = ref(false);
 const showEditProject = ref(false);
 const showCalendar = ref(false);
+const showFilters = ref(false);
 const showSaveViewModal = ref(false);
 const { trackTodoEvent, trackProjectEvent } = useAnalytics();
 
